af6544dabd74f1377c202a82885a56bf62bea6ae
Kishore-Kumar K <kishore1362002@gmail.com>
2021-10-04 23:02:51 +0530

    Create bgchange.js

---
+++ bgchange.js
@@ -0,0 +1,71 @@
+function onimg()
+{
+  this.setAttribute("src","minip 1b.png")
+}
+function outimg()
+{
+  this.setAttribute("src","minip 1.png")
+}
+
+function onimg1()
+{
+  this.setAttribute("src","minip 2b.png")
+}
+function outimg1()
+{
+  this.setAttribute("src","minip 2.png")
+}
+
+function onimg2()
+{
+  this.setAttribute("src","minip 3b.png")
+}
+function outimg2()
+{
+  this.setAttribute("src","minip 3.png")
+}
+
+function onimg3()
+{
+  this.setAttribute("src","minip 4b.png")
+}
+function outimg3()
+{
+  this.setAttribute("src","minip 4.png")
+}
+
+function onimg4()
+{
+  this.setAttribute("src","minip 5b.png")
+}
+function outimg4()
+{
+  this.setAttribute("src","minip 5.png")
+}
+
+function onimg5()
+{
+  this.setAttribute("src","minip 6b.png")
+}
+function outimg5()
+{
+  this.setAttribute("src","minip 6.png")
+}
+
+function onimg6()
+{
+  this.setAttribute("src","minip 7b.png")
+}
+function outimg6()
+{
+  this.setAttribute("src","minip 7.png")
+}
+
+function onimg7()
+{
+  this.setAttribute("src","minip 8b.png")
+}
+function outimg7()
+{
+  this.setAttribute("src","minip 8.png")
+}
